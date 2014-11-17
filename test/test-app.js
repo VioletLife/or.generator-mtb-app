c3a757215f9b018844f3377e3324d8a2fcbf5b4c
岑安 <cenan.chr@alibaba-inc.com>
2014-11-17 17:59:51 +0800

    init mtb-app

---
+++ test/test-app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var mockery = require('mockery');
+var os = require('os');
+
+describe('generator:app', function () {
+  before(function (done) {
+    mockery.enable({ warnOnUnregistered: false });
+    mockery.registerMock('github', function () {
+      return {
+        user: {
+          getFrom: function (data, cb) {
+            cb(null, JSON.stringify({
+              name: 'Tyrion Lannister',
+              email: 'imp@casterlyrock.com',
+              html_url: 'https://github.com/imp'
+            }));
+          }
+        }
+      };
+    });
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), '/yeoman-test'))
+      .withOptions({ 'skip-install': true })
+      .withPrompts({
+        githubUser: 'imp',
+        generatorName: 'temp',
+        pkgName: false
+      })
+      .on('end', done);
+  });
+
+  after(function () {
+    mockery.disable();
+  });
+
+  it('creates files', function () {
+    var expected = [
+      '.yo-rc.json',
+      '.gitignore',
+      '.gitattributes',
+      '.jshintrc',
+      'app/index.js',
+      'app/templates/_package.json',
+      'app/templates/_bower.json',
+    ];
+    assert.file(expected);
+  });
+
+  it('fills package.json with correct information', function () {
+    assert.fileContent('package.json',  /"name": "generator-temp"/);
+  });
+
+  it('setup travis.CI config', function () {
+    assert.fileContent(
+      '.travis.yml',
+      /if \[ "\$currentfolder" != 'generator-temp' \]; then cd .. \&\& eval "mv \$currentfolder generator-temp" && cd generator-temp; fi/
+    );
+  });
+});
---
+++ test/test-subgenerator.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var os = require('os');
+
+describe('generator:app', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../subgenerator'))
+      .inDir(path.join(os.tmpdir(), '/yeoman-test'), function (dir) {
+        fs.writeFileSync(path.join(dir, 'package.json'), '{"name": "generator-foo", "files":[]}');
+      })
+      .withArguments(['foo', '--force'])
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    var expected = [
+      'foo/index.js',
+      'foo/templates/somefile.js',
+    ];
+    assert.file(expected);
+  });
+
+  it('update package.json file array', function () {
+    var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    assert.equal(pkg.files[0], 'foo');
+  });
+});
